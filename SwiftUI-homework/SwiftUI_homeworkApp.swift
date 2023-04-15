5d04d442d28c2ca6465f50233cefa32a7afd5645
Dina <di.shvarova@gmail.com>
2023-04-15 17:32:49 +0300

    Initial Commit

---
+++ SwiftUI-homework/SwiftUI_homeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_homeworkApp.swift
+//  SwiftUI-homework
+//
+//  Created by Дина Шварова on 15.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_homeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
